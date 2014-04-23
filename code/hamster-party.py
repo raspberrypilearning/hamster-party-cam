f69c7c4a5727d43b4a969b3671e066d84fd78d4c
Carrie Anne Philbin <missphilbin@gmail.com>
2014-04-23 05:35:29 +0100

    Update to new version of code

--- code/hamster-party.py
+++ code/hamster-party.py
@@ -1,13 +1,15 @@
-import pibrella, picamera, time, random, os, sys
+import pibrella, picamera, time, random, os
 
-colours = [pibrella.light.red, pibrella.light.amber, pibrella.light.green]
+colours = [pibrella.light.red,
+           pibrella.light.amber,
+           pibrella.light.green]
 
 def disco():
-	for i  in range(10):
-    	result = random.choice(colours)
-    	result.on()
-    	time.sleep(0.2)
-    	result.off()
+    for i in range(25):
+        result = random.choice(colours)
+        result.on()
+        time.sleep(0.2)
+        result.off()
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
@@ -18,5 +20,6 @@ with picamera.PiCamera() as camera:
             print("Party!")
             os.system('omxplayer hamsterdance.mp3 &')
             disco()
+            time.sleep(10)
             pic += 1
-        time.sleep(0.5)
+        time.sleep(0.01)    
